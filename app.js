3654bdadfada3a34c98b88e473b534078634c472
twugo <hm17031@student.miyazaki-u.ac.jp>
2020-03-15 21:48:11 +0900

    Update:メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 'use strict';
+
 /**
- * フィボナッチ数列の n 番目の値を返す関数。
- * @param {Number} n 数列の何番目か
- * @return {Number} 数列の n 番目の値
+ * フィボナッチ数処理のためのクラス
  */
-function fib(n){
-    if(n === 0){
-        return 0;
-    }else if(n === 1){
-        return 1;
-    }else{
-        return fib(n-1) + fib(n-2);
+class Fib{
+    /**
+     * メモ化用変数の用意
+     */
+    constructor(){
+        this.memo = new Map();
+        this.memo.set(0, 0);
+        this.memo.set(1, 1);
+    }
+    
+    /**
+     * フィボナッチ数列の n 番目の値を返す関数。
+     * @param {Number} n 数列の何番目か
+     * @return {Number} 数列の n 番目の値
+     */
+    fib(n){
+        if(this.memo.has(n)){
+            return this.memo.get(n);
+        }
+        const value = this.fib(n-1) + this.fib(n-2);
+        this.memo.set(n, value);
+        return value;
     }
 }
 
 const length = 40;
+const fibonacchi = new Fib();
 for(let i = 0; i <= length; i++){
-    console.log(fib(i));
+    console.log(fibonacchi.fib(i));
 }
